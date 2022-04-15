993df9183d3db65efb0a699d680c46db7a1b448d
hayaten0415 <hayaten415@gmail.com>
2022-04-15 14:58:33 +0900

    localStorage mount

--- src/reducers/operationLogs.js
+++ src/reducers/operationLogs.js
@@ -1,7 +1,6 @@
 import {ADD_OPERATION_LOG, DELETE_ALL_OPERATION_LOGS} from '../actions'
 
 const operationLogs = (state = [], action) => {
-  console.log(state)
   switch (action.type) {
     case ADD_OPERATION_LOG:
       const operationLog = {
